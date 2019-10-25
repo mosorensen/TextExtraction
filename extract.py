082edf3601d619791920be02620591753c0c1341
yanndecressin <Yann.Decressin@chicagobooth.edu>
2019-10-25 18:42:38 -0500

    wrote parse_table, which_table, and cleaned loop

--- extract.py
+++ extract.py
@@ -46,75 +46,114 @@ Notes:
 from docx import Document
 import pandas as pd
 
+
+comp_cat = ["intellectual ability", "personal effectiveness", 
+            "team leadership", "interpersonal effectiveness", 
+            "contextual skills/experience"]
+
+document = Document("C:/Users/ydecress/Desktop/Cleaning_Small.docx")
+
+tabs = document.tables
+comp_tab = tabs[5]
+
+for para in document.paragraphs:
+    print(para.text)
+    
+# loops through the table and decides what table it is based on if any of
+# its cells contains certain strings. To add robustness, we could
+# check if multiple cells fulfill certain criteria.
+def which_table(table):
+    table_name="other"
+    num_comp = 0
+    for row in table.rows:
+        last_text = ""
+        for cell in row.cells:
+            curr_text = cell.text
+            #Check 1: does the table give identifying information
+            if "Candidate Name" == curr_text.strip():
+                table_name="info"
+            #Check 2: does the table give competency information
+            if curr_text.lower() in comp_cat:
+                if last_text=="":
+                    last_text = curr_text
+                else:
+                    if last_text==curr_text: #a merged cell is interpreted as two cells with the same contents
+                        num_comp+=1 #keep track of the number of listed competencies
+                    else:
+                        last_text=curr_text #if not a merged cell, update last_text
+    if num_comp == 5: #the table must identify all five competency categories
+        table_name = "competencies"
+            
+    return table_name
+
+i=which_table(comp_tab)
+
+#To add robustness, we can add a variable to let the table parser know what type
+#    of table it is currently dealing with.
+def parse_table(table, doc_id):
+    table_dict = {}
+    table_data = pd.DataFrame({'doc_id':[doc_id]})
+    for i, row in enumerate(table.rows): #loop through all of the rows
+        text = (cell.text for cell in row.cells) #converts all tables cells to text  
+        text = tuple(text) #need to convert from row object to a tuple
+        j=0 #let j index over the entries in the row tuple
+        while j < len(text)-1: #while we have at least one additional entry in the row
+            key = text[j]
+            if len(key) == 0: #if the current cell is empty, it can't be a key, so skip it
+                j+=1
+                continue
+            entries = text[j+1] #the keys entry is next to it in the table
+            j=j+2 #the next key is after the current key's entry...so two indices away
+            if key==entries: #if the cell if a merged cell, skip it
+                continue  
+            # add the keys and entries into a table specific dictionary and dataframe
+            table_dict[key] = entries 
+            table_data[key] = entries     
+    return (table_dict, table_data)    
+
+
+tables_to_parse = ["info", "competencies"]
 doc_paths = ["C:/Users/ydecress/Desktop/Cleaning_Small.docx", "C:/Users/ydecress/Desktop/Cleaning_Small_Copy.docx"]
 for doc_id in range(len(doc_paths)):
     #first read in the given document
     print(doc_id)
     path = doc_paths[doc_id]
     document = Document(path)
-    dict_tables = {} #dictionary of all tables
-    data_tables = pd.DataFrame({'doc_id':[doc_id], 'doc_path':[path]})
+    doc_dict = {} #dictionary of all tables
+    doc_data = pd.DataFrame({'doc_id':[doc_id], 'doc_path':[path]})
     table_id = 0 #tracks the table index
     for table in document.tables:
-        table_dict = {}
         table_id += 1
-        #i indexes the rows, row is the row object
-        for i, row in enumerate(table.rows):
-            text = (cell.text for cell in row.cells) #converts all tables cells to text  
-            text = tuple(text) #need to convert from row object to a tuple
-            if i == 0: # Establish the table name based on first row
-                table_key = text[0]
-                if table_id==1: #if this is the first table in the document
-                    table_key = "info"
-                table_name = "table_name_" + str(table_id)
-                data_tables[table_name] = table_key
-                table_dict[table_name] = table_key
-            
-            elif i !=0 : # Establishing the contents in the rows afterwards
-                j=0 #let j index over the entries in the row tuple
-                while j < len(text)-1: #while we have at least one more entry in the row
-                    key = text[j]
-                    if len(key) == 0: #if the current cell is empty, skip it
-                        j+=1
-                        del key
-                        continue
-                    entries = text[j+1]
-                    j=j+2 #the next key is after the current key's entry...so two indices away
-                    if key==entries:
-                        del key, entries
-                        continue  #if the cell if a merged cell, skip it
-                    table_dict[key] = entries # add the keys and entries into a table specific dictionary
-                    data_tables[key] = entries
-                    del key, entries
-                #this code below saves the entries as an array
-                #key = text[0]
-                #entries = text[1:]
-                #print(key)
-                #print(entries)
-                
-        # add the table to the overall document dictionary...just keep adding columns to the dataframe
-        dict_tables[table_key]=table_dict
-        del table_key
-    
+        table_type = which_table(table)
+        if table_type not in tables_to_parse:
+            print(table_type, "not parsed")
+            continue
+        print(table_type, "parsing")
+        table_dict, table_data = parse_table(table, doc_id) #parse the table and its dictionary and dataframe form
+        # add the table to the document dictionary and document table
+        doc_dict[table_type]=table_dict 
+        doc_data = pd.merge(doc_data, table_data, on='doc_id', how='outer')
+    candidate_name=doc_dict['info']['Candidate Name']
     #if this was the first document, we need to create the master dataframe
     #each row will be one executive and each column will contain a variable
-    #value for that executive. The first column contains the document id, the 
-    #second column contains the filename
+    #each column gets a unique doc_id and path to document
     if doc_id==0: 
-        data_master = data_tables
-        dict_master = {doc_id:dict_tables}
+        data_master = doc_data
+        dict_master = {candidate_name:doc_dict}
     else:
-        data_master = data_master.append(data_tables)
-        dict_master[doc_id] = dict_tables
+        data_master = data_master.append(doc_data)
+        dict_master[candidate_name] = doc_dict
 
 
 dict_master
 data_master
 
 
-cover_page = all_tables['SmartAssessment Report']
-cover_page_df = pd.DataFrame.from_dict(cover_page, 'columns')
-cover_page_df
+
+
+
+
+
 
 
 
